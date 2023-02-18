d399b58b9ab14a58f999a87e7cce9d4022d3bd5e
Ivan <mr.berdnikov1998@mail.ru>
2023-02-18 18:16:00 +0300

    added Java compile 'UTF-8' in build.gradle and create TextBoxTests on demoqa

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,35 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void configuration() {
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        String userName = "userName";
+        String userEmail = "userEmail@mail.ru";
+        String currentAddress = "currentAddress";
+        String permanentAddress = "permanentAddress";
+        open("/text-box");
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+        $("#output #name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+    }
+}
