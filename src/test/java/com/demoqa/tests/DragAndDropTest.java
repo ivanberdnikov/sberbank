488644c2db5fedccbb7beb010864a7ada5200b0f
Ivan <mr.berdnikov1998@mail.ru>
2023-02-20 19:50:09 +0300

    Создал тест Drag and Drop

---
+++ src/test/java/com/demoqa/tests/DragAndDropTest.java
@@ -0,0 +1,19 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+    @Test
+    public void dragAndDrop() {
+        SelenideElement columnA = $("#column-a"), columnB = $("#column-b");
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        columnA.dragAndDropTo(columnB);
+        columnA.shouldHave(Condition.text("B"));
+    }
+}
